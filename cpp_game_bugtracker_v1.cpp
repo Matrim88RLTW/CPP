0e813564c406af53ebe9286464fcf0b147b81229
Mateo <101015398+Matrim88RLTW@users.noreply.github.com>
2023-01-03 21:52:35 -0700

    Update cpp_game_bugtracker_v1.cpp
    Adjusting known bug array

--- cpp_game_bugtracker_v1.cpp
+++ cpp_game_bugtracker_v1.cpp
@@ -48,7 +48,3 @@ class BugTracker {
   // Create an instance of the bug tracker
   BugTracker tracker;
 
-  // Add some bugs to the tracker
-  tracker.AddBug({"Crash on launch", "open", "John", "Alice"});
-  tracker.AddBug({"Wrong result", "open", "Mike", "Bob"});
-  tracker.PrintBugs();
